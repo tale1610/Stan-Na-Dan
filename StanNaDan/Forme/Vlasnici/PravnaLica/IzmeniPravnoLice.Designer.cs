44c570192eb1dde5dd4453f8ac2b3bd2a36b5490
Dimitrije Najdanovic <n.dimitrije@elfak.rs>
2024-05-27 23:36:36 +0200

    GotoveFormeIpakNisuuu

---
+++ StanNaDan/Forme/Vlasnici/PravnaLica/IzmeniPravnoLice.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace StanNaDan.Forme.Vlasnici.PravnaLica
+{
+    partial class IzmeniPravnoLice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnIzmeniPravnoLice = new Button();
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            tbEmailKontaktOsobe = new TextBox();
+            tbImeKontaktOsobe = new TextBox();
+            tbAdresa = new TextBox();
+            tbNaziv = new TextBox();
+            tbPIB = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            groupBox2 = new GroupBox();
+            tbBrojBankovnogRacuna = new TextBox();
+            tbBanka = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnIzmeniPravnoLice
+            // 
+            btnIzmeniPravnoLice.Location = new Point(104, 490);
+            btnIzmeniPravnoLice.Name = "btnIzmeniPravnoLice";
+            btnIzmeniPravnoLice.Size = new Size(245, 29);
+            btnIzmeniPravnoLice.TabIndex = 6;
+            btnIzmeniPravnoLice.Text = "Izmeni Pravno Lice";
+            btnIzmeniPravnoLice.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox3);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(474, 472);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Podaci o pravnom licu";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(tbEmailKontaktOsobe);
+            groupBox3.Controls.Add(tbImeKontaktOsobe);
+            groupBox3.Controls.Add(tbAdresa);
+            groupBox3.Controls.Add(tbNaziv);
+            groupBox3.Controls.Add(tbPIB);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(label6);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Location = new Point(6, 183);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(448, 276);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Pravni podaci";
+            // 
+            // tbEmailKontaktOsobe
+            // 
+            tbEmailKontaktOsobe.Location = new Point(194, 221);
+            tbEmailKontaktOsobe.Name = "tbEmailKontaktOsobe";
+            tbEmailKontaktOsobe.Size = new Size(248, 27);
+            tbEmailKontaktOsobe.TabIndex = 13;
+            // 
+            // tbImeKontaktOsobe
+            // 
+            tbImeKontaktOsobe.Location = new Point(194, 174);
+            tbImeKontaktOsobe.Name = "tbImeKontaktOsobe";
+            tbImeKontaktOsobe.Size = new Size(248, 27);
+            tbImeKontaktOsobe.TabIndex = 12;
+            // 
+            // tbAdresa
+            // 
+            tbAdresa.Location = new Point(194, 129);
+            tbAdresa.Name = "tbAdresa";
+            tbAdresa.Size = new Size(248, 27);
+            tbAdresa.TabIndex = 11;
+            // 
+            // tbNaziv
+            // 
+            tbNaziv.Location = new Point(194, 81);
+            tbNaziv.Name = "tbNaziv";
+            tbNaziv.Size = new Size(248, 27);
+            tbNaziv.TabIndex = 10;
+            // 
+            // tbPIB
+            // 
+            tbPIB.Location = new Point(194, 37);
+            tbPIB.Name = "tbPIB";
+            tbPIB.Size = new Size(248, 27);
+            tbPIB.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 224);
+            label7.Name = "label7";
+            label7.Size = new Size(151, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Email Kontakt Osobe:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 177);
+            label6.Name = "label6";
+            label6.Size = new Size(139, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Ime Kontakt Osobe:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 132);
+            label5.Name = "label5";
+            label5.Size = new Size(110, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Adresa Sedista:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 84);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Naziv:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 20);
+            label3.TabIndex = 0;
+            label3.Text = "PIB:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(tbBrojBankovnogRacuna);
+            groupBox2.Controls.Add(tbBanka);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(6, 42);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(448, 124);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Bankovni podaci";
+            // 
+            // tbBrojBankovnogRacuna
+            // 
+            tbBrojBankovnogRacuna.Location = new Point(194, 84);
+            tbBrojBankovnogRacuna.Name = "tbBrojBankovnogRacuna";
+            tbBrojBankovnogRacuna.Size = new Size(248, 27);
+            tbBrojBankovnogRacuna.TabIndex = 3;
+            // 
+            // tbBanka
+            // 
+            tbBanka.Location = new Point(194, 38);
+            tbBanka.Name = "tbBanka";
+            tbBanka.Size = new Size(248, 27);
+            tbBanka.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(169, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Broj Bankovnog Racuna:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Banka:";
+            // 
+            // IzmeniPravnoLice
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(494, 533);
+            Controls.Add(btnIzmeniPravnoLice);
+            Controls.Add(groupBox1);
+            Name = "IzmeniPravnoLice";
+            Text = "IzmeniPravnoLice";
+            groupBox1.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnIzmeniPravnoLice;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private TextBox tbEmailKontaktOsobe;
+        private TextBox tbImeKontaktOsobe;
+        private TextBox tbAdresa;
+        private TextBox tbNaziv;
+        private TextBox tbPIB;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private GroupBox groupBox2;
+        private TextBox tbBrojBankovnogRacuna;
+        private TextBox tbBanka;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ StanNaDan/Forme/Zaposleni/Sefovi/IzmeniSefa.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace StanNaDan.Forme.Zaposleni.Sefovi
+{
+    partial class IzmeniSefa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnIzmeniSefa = new Button();
+            groupBox1 = new GroupBox();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker1 = new DateTimePicker();
+            tbPrezime = new TextBox();
+            tbIme = new TextBox();
+            tbMBR = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnIzmeniSefa
+            // 
+            btnIzmeniSefa.Location = new Point(83, 316);
+            btnIzmeniSefa.Name = "btnIzmeniSefa";
+            btnIzmeniSefa.Size = new Size(270, 29);
+            btnIzmeniSefa.TabIndex = 5;
+            btnIzmeniSefa.Text = "Izmeni Sefa";
+            btnIzmeniSefa.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dateTimePicker2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(tbPrezime);
+            groupBox1.Controls.Add(tbIme);
+            groupBox1.Controls.Add(tbMBR);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(438, 298);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Podaci o sefu";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(195, 240);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(216, 27);
+            dateTimePicker2.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(195, 186);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(216, 27);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // tbPrezime
+            // 
+            tbPrezime.Location = new Point(195, 134);
+            tbPrezime.Name = "tbPrezime";
+            tbPrezime.Size = new Size(216, 27);
+            tbPrezime.TabIndex = 7;
+            // 
+            // tbIme
+            // 
+            tbIme.Location = new Point(195, 87);
+            tbIme.Name = "tbIme";
+            tbIme.Size = new Size(216, 27);
+            tbIme.TabIndex = 6;
+            // 
+            // tbMBR
+            // 
+            tbMBR.Location = new Point(195, 43);
+            tbMBR.Name = "tbMBR";
+            tbMBR.Size = new Size(216, 27);
+            tbMBR.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(24, 245);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Datum Postavljanja";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Datum Zaposlenja";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 141);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Prezime";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(34, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ime";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 20);
+            label1.TabIndex = 0;
+            label1.Text = "MBR";
+            // 
+            // IzmeniSefa
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(461, 357);
+            Controls.Add(btnIzmeniSefa);
+            Controls.Add(groupBox1);
+            Name = "IzmeniSefa";
+            Text = "IzmeniSefa";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnIzmeniSefa;
+        private GroupBox groupBox1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox tbPrezime;
+        private TextBox tbIme;
+        private TextBox tbMBR;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
