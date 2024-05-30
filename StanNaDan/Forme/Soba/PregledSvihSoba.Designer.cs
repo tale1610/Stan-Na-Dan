4747130f1a43b4fb2d2604abb7397214c62f210d
taskov_ <taskovvukan@gmail.com>
2024-05-30 02:37:10 +0200

    izgled forme

--- StanNaDan/Forme/Soba/PregledSvihSoba.Designer.cs
+++ StanNaDan/Forme/Soba/PregledSvihSoba.Designer.cs
@@ -41,10 +41,9 @@
             listaSoba.Columns.AddRange(new ColumnHeader[] { IdNekretnine, IdSobe });
             listaSoba.FullRowSelect = true;
             listaSoba.GridLines = true;
-            listaSoba.Location = new Point(10, 9);
-            listaSoba.Margin = new Padding(3, 2, 3, 2);
+            listaSoba.Location = new Point(11, 12);
             listaSoba.Name = "listaSoba";
-            listaSoba.Size = new Size(214, 309);
+            listaSoba.Size = new Size(244, 411);
             listaSoba.TabIndex = 1;
             listaSoba.UseCompatibleStateImageBehavior = false;
             listaSoba.View = View.Details;
@@ -61,10 +60,9 @@
             // 
             // btnUpravljanje
             // 
-            btnUpravljanje.Location = new Point(245, 11);
-            btnUpravljanje.Margin = new Padding(3, 2, 3, 2);
+            btnUpravljanje.Location = new Point(280, 15);
             btnUpravljanje.Name = "btnUpravljanje";
-            btnUpravljanje.Size = new Size(164, 41);
+            btnUpravljanje.Size = new Size(187, 55);
             btnUpravljanje.TabIndex = 2;
             btnUpravljanje.Text = "Upravljanje nekretninama i sobama";
             btnUpravljanje.UseVisualStyleBackColor = true;
@@ -72,10 +70,9 @@
             // 
             // btnZajednickeProstorije
             // 
-            btnZajednickeProstorije.Location = new Point(245, 76);
-            btnZajednickeProstorije.Margin = new Padding(3, 2, 3, 2);
+            btnZajednickeProstorije.Location = new Point(280, 101);
             btnZajednickeProstorije.Name = "btnZajednickeProstorije";
-            btnZajednickeProstorije.Size = new Size(164, 41);
+            btnZajednickeProstorije.Size = new Size(187, 55);
             btnZajednickeProstorije.TabIndex = 5;
             btnZajednickeProstorije.Text = "Prikazi zajednicke prostorije";
             btnZajednickeProstorije.UseVisualStyleBackColor = true;
@@ -83,9 +80,10 @@
             // 
             // btnIzaberiSobu
             // 
-            btnIzaberiSobu.Location = new Point(282, 167);
+            btnIzaberiSobu.Location = new Point(280, 184);
+            btnIzaberiSobu.Margin = new Padding(3, 4, 3, 4);
             btnIzaberiSobu.Name = "btnIzaberiSobu";
-            btnIzaberiSobu.Size = new Size(188, 29);
+            btnIzaberiSobu.Size = new Size(187, 56);
             btnIzaberiSobu.TabIndex = 6;
             btnIzaberiSobu.Text = "Izaberi Sobu";
             btnIzaberiSobu.UseVisualStyleBackColor = true;
@@ -93,15 +91,13 @@
             // 
             // PregledSvihSoba
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(421, 327);
-            ClientSize = new Size(481, 446);
+            ClientSize = new Size(485, 441);
             Controls.Add(btnIzaberiSobu);
             Controls.Add(btnZajednickeProstorije);
             Controls.Add(btnUpravljanje);
             Controls.Add(listaSoba);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "PregledSvihSoba";
             Text = "PregledSvihSoba";
             Load += PregledSvihSoba_Load;
